76181cf7a1a6495197f6a47ad266f322a600a278
Oceanswave <sean@baristalabs.io>
2019-05-18 13:58:40 -0400

    Add coverlet

---
+++ test/Paperspace.Tests/Clients/MachinesClientTests.cs
@@ -0,0 +1,29 @@
+﻿namespace Paperspace.Tests.Clients
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+    using Newtonsoft.Json;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    [TestClass]
+    public class MachinesClientTests
+    {
+        [TestMethod]
+        public async Task MachinesClient_List_HappyPath()
+        {
+            var connection = new Mock<IConnection>();
+            connection.Setup(c => c.Get<IList<Machine>>(ApiUrls.MachinesList(), null))
+                .ReturnsAsync(() =>
+                {
+                    var json = System.IO.File.ReadAllText("./Fixtures/ListMachines.json");
+                    return JsonConvert.DeserializeObject<IList<Machine>>(json);
+                });
+
+            var machinesClient = new MachinesClient(connection.Object);
+            var result = await machinesClient.List();
+
+            Assert.AreEqual(result.Count, 1);
+        }
+    }
+}
