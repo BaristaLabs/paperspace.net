d7d2e1a78563ab39b460cf1b60aac4098f66fbc8
Oceanswave <sean@baristalabs.io>
2019-07-05 18:27:40 -0400

    Implement Resource Delegation Client

--- src/Paperspace/Clients/IResourceDelegationsClient.cs
+++ src/Paperspace/Clients/IResourceDelegationsClient.cs
@@ -1,6 +1,15 @@
 ﻿namespace Paperspace
 {
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
     public interface IResourceDelegationsClient
     {
+        /// <summary>
+        /// Create resourceDelegation with limited access, e.g. for users who needs to stream a machine you created. The create method takes a delegation object as the only argument with resource name as key and an object with ids to list resource ids to give access to.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        Task<IList<ResourceDelegation>> Create(CreateResourceDelegationRequest request);
     }
 }
